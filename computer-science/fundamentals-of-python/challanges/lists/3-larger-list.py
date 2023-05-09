43df8604e6291c29c6f4c456825c4bf85abf5cc7
Rebekah <macrosystem@proton.me>
2023-05-09 23:34:50 +0200

    Update 3-larger-list.py

--- computer-science/fundamentals-of-python/challanges/lists/3-larger-list.py
+++ computer-science/fundamentals-of-python/challanges/lists/3-larger-list.py
@@ -2,10 +2,15 @@
 Write a function named larger_list that has two parameters named my_list1 and my_list2.
 
 The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of my_list1.
-
-#Write your function here
 """
+#Write your function here
+
+def larger_list(a,b):
+  la, lb  = len(a), len(b)
+  if la < lb:
+   return b[-1]
+  elif la => lb: #can just be written "else" but this is more readable
+   return a[-1]
+
 #Uncomment the line below when your function is done
-
-
-#print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
