858e79040ae1900949f07d73a01c594404654f55
Rebekah <macrosystem@proton.me>
2023-05-13 20:43:37 +0200

    Update 4-more-than-n.py

--- computer-science/fundamentals-of-python/challanges/lists/4-more-than-n.py
+++ computer-science/fundamentals-of-python/challanges/lists/4-more-than-n.py
@@ -3,3 +3,8 @@ Create a function named more_than_n that has three parameters named my_list, ite
 
 The function should return True if item appears in the list more than n times. The function should return False otherwise.
 """
+#Write your function here
+
+
+#Uncomment the line below when your function is done
+#print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
