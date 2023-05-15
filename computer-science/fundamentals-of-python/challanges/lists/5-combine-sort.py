233fc2b7063208679e2d7cf304f4bac3523f0596
Rebekah <macrosystem@proton.me>
2023-05-15 12:23:13 +0200

    Update 5-combine-sort.py

--- computer-science/fundamentals-of-python/challanges/lists/5-combine-sort.py
+++ computer-science/fundamentals-of-python/challanges/lists/5-combine-sort.py
@@ -9,3 +9,6 @@ def combine_sort(my_list1, my_list2):
 
 #Uncomment the line below when your function is done
 print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+#alternative solution, seems to be atleast as clear and not include the extra work of writing a function?:
+print(combine_sort([4, 10, 2, 5] + [-10, 2, 5, 10]))
