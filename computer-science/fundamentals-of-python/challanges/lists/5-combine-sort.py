f401b9bb8f2609d3b6d65dd600186e0ef1578e65
Rebekah <macrosystem@proton.me>
2023-05-15 12:19:22 +0200

    Update 5-combine-sort.py

--- computer-science/fundamentals-of-python/challanges/lists/5-combine-sort.py
+++ computer-science/fundamentals-of-python/challanges/lists/5-combine-sort.py
@@ -5,7 +5,7 @@ The function should combine these two lists into one new list and sort the resul
 """
 #Write your function here
 def combine_sort(my_list1, my_list2):
-  return my_list1 + my_list2
+  return sorted( my_list1 + my_list2 )
 
 #Uncomment the line below when your function is done
-#print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
