3d14cdb3b769a740fb2aedca903e71d6542f7428
Rebekah <macrosystem@proton.me>
2023-05-18 13:42:17 +0200

    Update 2-remove-middle.py

--- computer-science/fundamentals-of-python/challanges/lists-advanced/2-remove-middle.py
+++ computer-science/fundamentals-of-python/challanges/lists-advanced/2-remove-middle.py
@@ -7,3 +7,10 @@ For example, the following code should return [4, 23, 42] because elements at in
 
 remove_middle([4, 8 , 15, 16, 23, 42], 1, 3)
 """
+#Write your function here
+
+remove_middle(my_list, start, end):
+  pass
+
+#Uncomment the line below when your function is done
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
