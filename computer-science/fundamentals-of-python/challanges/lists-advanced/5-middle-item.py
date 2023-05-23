e3a707a5eeefffd0b31ec2fa3b72c4cb00145cd0
Rebekah <macrosystem@proton.me>
2023-05-23 23:21:19 +0200

    Update 5-middle-item.py

--- computer-science/fundamentals-of-python/challanges/lists-advanced/5-middle-item.py
+++ computer-science/fundamentals-of-python/challanges/lists-advanced/5-middle-item.py
@@ -3,3 +3,9 @@ Create a function called middle_element that has one parameter named my_list.
 
 If there are an odd number of elements in my_list, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
 """
+
+#Write your function here
+
+
+#Uncomment the line below when your function is done
+#print(middle_element([5, 2, -10, -4, 4, 5]))
