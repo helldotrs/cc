d30814281f71d87201b5162e1390c17796d14156
Rebekah <macrosystem@proton.me>
2023-05-26 18:06:26 +0200

    Update 5-middle-item.py

--- computer-science/fundamentals-of-python/challanges/lists-advanced/5-middle-item.py
+++ computer-science/fundamentals-of-python/challanges/lists-advanced/5-middle-item.py
@@ -6,8 +6,14 @@ If there are an odd number of elements in my_list, the function should return th
 
 #Write your function here
 def middle_element(my_list):
-  
+  my_len = len(my_list)
+  if my_len % 2 == 0:
+    return my_list[my_len/2]
+  else:
+    return my_list[my_len/2]
 
 
 #Uncomment the line below when your function is done
+print(middle_element(10, 20, 30)
+print(middle_element(10, 20, 30, 40)
 #print(middle_element([5, 2, -10, -4, 4, 5]))
