e8908b4a23368e98f0da366cd101fa4dc26cd562
Rebekah <macrosystem@proton.me>
2023-06-03 23:23:27 +0200

    Update divisible-by-ten.py

--- computer-science/fundamentals-of-python/challanges/loops/divisible-by-ten.py
+++ computer-science/fundamentals-of-python/challanges/loops/divisible-by-ten.py
@@ -1,5 +1,11 @@
 #Write your function here
 
+def devisible_by_ten(in_list)
+  count = 0
+  for a in in_list:
+    if not 10 % 0 == 0:
+      count += 1
+
 
 #Uncomment the line below when your function is done
 #print(divisible_by_ten([20, 25, 30, 35, 40]))
