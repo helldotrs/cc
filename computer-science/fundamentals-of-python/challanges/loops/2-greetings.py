32fd33381fb47010b61314dd949891dea0a4375c
Rebekah <macrosystem@proton.me>
2023-06-05 03:42:34 +0200

    Update 2-greetings.py

--- computer-science/fundamentals-of-python/challanges/loops/2-greetings.py
+++ computer-science/fundamentals-of-python/challanges/loops/2-greetings.py
@@ -1,5 +1,10 @@
 #Write your function here
 
+def add_greetings(a):
+  c = []
+  for b in a:
+    c.append[f"Hola {c}!"]
+  return c
 
 #Uncomment the line below when your function is done
 #print(add_greetings(["Owen", "Max", "Sophie"]))
