37db5a4600c02c6be462b7ca180e7daf8f2aa110
Rebekah <macrosystem@proton.me>
2023-06-06 14:58:34 +0200

    Update 2-greetings.py

--- computer-science/fundamentals-of-python/challanges/loops/2-greetings.py
+++ computer-science/fundamentals-of-python/challanges/loops/2-greetings.py
@@ -3,7 +3,7 @@
 def add_greetings(a):
   c = []
   for b in a:
-    c.append[f"Hola {c}!"]
+    c.append[f"Hola {b}!"] #writing c here instead of b is a good lesson why naming matter
   return c
 
 #Uncomment the line below when your function is done
