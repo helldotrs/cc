c42a190ffd99ec3fb61eb9aa213366e17300724e
Rebekah <macrosystem@proton.me>
2023-06-08 12:57:39 +0200

    Update 3-delete-even-starting-numbers.py

--- computer-science/fundamentals-of-python/challanges/loops/3-delete-even-starting-numbers.py
+++ computer-science/fundamentals-of-python/challanges/loops/3-delete-even-starting-numbers.py
@@ -1,8 +1,13 @@
 #Write your function here
+
 def delete_starting_evens(my_list):
   if len(my_list):
     for a in my_list:
-      pass
+      if not 2 % 0 == 0:
+        my_list = my_list[1:]
+        
+  else: #else for clarity
+    return my_list
 
 #Uncomment the lines below when your function is done
 #print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
