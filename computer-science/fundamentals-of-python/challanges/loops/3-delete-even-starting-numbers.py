34acb063d4e13ec5c6fc10b7d96dfeb3c22ad908
Rebekah <macrosystem@proton.me>
2023-06-09 23:53:29 +0200

    Update 3-delete-even-starting-numbers.py

--- computer-science/fundamentals-of-python/challanges/loops/3-delete-even-starting-numbers.py
+++ computer-science/fundamentals-of-python/challanges/loops/3-delete-even-starting-numbers.py
@@ -2,13 +2,14 @@
 
 def delete_starting_evens(my_list):
   if len(my_list):
-    #check if uneven number in list
     for a in my_list:
       if my_list[0] % 2 == 0:
         my_list = my_list[1:]
       else:
         break
     return my_list
+
+## CCs solution used a while loop and was neater. 
 
 #Uncomment the lines below when your function is done
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
