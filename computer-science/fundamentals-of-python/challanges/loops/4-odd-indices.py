9ffa2a51dab62cf22e26f1e40a5fe10bec5ed7f2
Rebekah <macrosystem@proton.me>
2023-06-13 01:19:37 +0200

    Update 4-odd-indices.py

--- computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
+++ computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
@@ -1,5 +1,9 @@
 #Write your function here
 
+odd_indices(my_list):
+  odd_list = []
+  for a in my_list where a/2 != 0:
+    odd_list.append(a)
 
 #Uncomment the line below when your function is done
 #print(odd_indices([4, 3, 7, 10, 11, -2]))
