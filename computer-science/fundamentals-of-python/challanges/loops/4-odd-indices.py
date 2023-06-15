585eb61adecdb3929648c6bb8488c5e2facac9f0
Rebekah <macrosystem@proton.me>
2023-06-15 13:08:16 +0200

    Update 4-odd-indices.py

--- computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
+++ computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
@@ -1,7 +1,7 @@
 #Write your function here
 
 def odd_indices(my_list):
-  odd_list = [a for a in my_list where if % 2 != 0]
+  odd_list = [my_list[a] for a in my_list where if % 2 != 0]
   return odd_list
 
 #Uncomment the line below when your function is done
