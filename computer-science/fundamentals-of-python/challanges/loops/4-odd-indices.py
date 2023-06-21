3a5a359994b5637fc34962a7dfd387be27e921dd
Rebekah <macrosystem@proton.me>
2023-06-21 15:19:21 +0200

    Update 4-odd-indices.py

--- computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
+++ computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
@@ -1,9 +1,11 @@
 #Write your function here
 
 def odd_indices(my_list):
-  odd_list = [sublist for sublist in sublists if sublist]
+  len_var   = len(my_list)
+  #odd_list = [sublist for sublist in sublists if sublist]
   """odd_list = [my_list[a] for a in my_list where if % 2 != 0]"""
-  return odd_list
+  #return odd_list
+  return len_var
 
 #Uncomment the line below when your function is done
 print(odd_indices([4, 3, 7, 10, 11, -2]))
