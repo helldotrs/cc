645b837b3255ceda09cc68c2ab80ec42896bbbb4
Rebekah <macrosystem@proton.me>
2023-06-23 01:57:16 +0200

    Update 4-odd-indices.py

--- computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
+++ computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
@@ -1,10 +1,7 @@
 #Write your function here
 
 def odd_indices(my_list):
-  out_list = []
-  for a in range (1, len(my_list), 2): #start, stop, step
-    out_list.append(my_list[a])
-  return out_list
+  return [my_list[a] for a in range(1, len(my_list), 2)]
 
 #Uncomment the line below when your function is done
 print(odd_indices([4, 3, 7, 10, 11, -2]))
