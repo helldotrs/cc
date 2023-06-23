482de78beb43628f2bfde6f100e58fa145546f57
Rebekah <macrosystem@proton.me>
2023-06-23 18:10:03 +0200

    Update 4-odd-indices.py

--- computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
+++ computer-science/fundamentals-of-python/challanges/loops/4-odd-indices.py
@@ -1,7 +1,7 @@
 #Write your function here
 
 def odd_indices(my_list):
-  return [my_list[a] for a in range(1, len(my_list), 2)]
+  return [my_list[a] for a in range(1, len(my_list), 2)] #1 to cut out the first even index (0), len() for end of range, 2 for every second (1 is default if nothing stated)
 
 #Uncomment the line below when your function is done
 print(odd_indices([4, 3, 7, 10, 11, -2]))
