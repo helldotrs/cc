e3c51dd119f3deef47536dd1426fde4616de736f
Rebekah <macrosystem@proton.me>
2023-06-25 03:01:29 +0200

    Update 5-exponents.py

--- computer-science/fundamentals-of-python/challanges/loops/5-exponents.py
+++ computer-science/fundamentals-of-python/challanges/loops/5-exponents.py
@@ -3,7 +3,8 @@
 def expontents(bases,powers):
   expo = []
   for a in range(0, len(bases)):
-    expo.append(a ** 
+    expo.append(bases[a] ** powers[a])
+  return expo
     
 
 #Uncomment the line below when your function is done
