439734cdbdf74ad9f2831964b846369f22aa146b
Rebekah <macrosystem@proton.me>
2023-06-29 01:20:34 +0200

    Update 1-larger-sum.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/1-larger-sum.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/1-larger-sum.py
@@ -1,7 +1,16 @@
 #Write your function here
 
-larger_sum(a,b):
-  pass
+def larger_sum(a,b):
+  sum_a = 0
+  sum_b = 0
+  for items in a:
+    sum_a += a
+  for items in b:
+    sum_b += b
+  if sum_a < sum_b:
+    return sum_b
+  else:
+    return sum_a
 
 #Uncomment the line below when your function is done
-#print(larger_sum([1, 9, 5], [2, 3, 7]))
+print(larger_sum([1, 9, 5], [2, 3, 7]))
