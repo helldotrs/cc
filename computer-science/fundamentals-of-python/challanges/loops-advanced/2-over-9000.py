0e612a426dca48a43e888a2518f7a8b3dc620ffe
Rebekah <macrosystem@proton.me>
2023-07-02 14:46:40 +0200

    Update 2-over-9000.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/2-over-9000.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/2-over-9000.py
@@ -1,5 +1,13 @@
 #Write your function here
 
+def over_nine_thousand[a]:
+  sum = 0
+  for b in a:
+    if sum > 9000:
+      break
+    sum += b
+  return sum
+
 
 #Uncomment the line below when your function is done
-#print(over_nine_thousand([8000, 900, 120, 5000]))
+print(over_nine_thousand([8000, 900, 120, 5000]))
