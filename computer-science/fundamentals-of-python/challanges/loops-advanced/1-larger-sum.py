f58b71d73b41f83d42d0fdd9b3b512db00540fbf
Rebekah <macrosystem@proton.me>
2023-07-02 14:40:44 +0200

    Update 1-larger-sum.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/1-larger-sum.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/1-larger-sum.py
@@ -12,7 +12,18 @@ def larger_sum(a,b):
   else:
     return sum_a
 
-# exact same solution as CC but with different names lmao
+# exact same solution as CC but with different names lmao:
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+  for number in lst1:
+    sum1 += number
+  for number in lst2:
+    sum2 += number
+  if sum1 >= sum2:
+    return lst1
+  else: 
+    return lst2
 
 #Uncomment the line below when your function is done
 print(larger_sum([1, 9, 5], [2, 3, 7]))
