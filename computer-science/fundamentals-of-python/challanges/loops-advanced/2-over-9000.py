13595eda8cfc0b6bb5cee9b798c23a0a70edaf72
Rebekah <macrosystem@proton.me>
2023-07-02 14:50:38 +0200

    Update 2-over-9000.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/2-over-9000.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/2-over-9000.py
@@ -10,6 +10,15 @@ def over_nine_thousand(a):
     sum += b
   return sum
 
+#their solution looks slightly different but returns the same value (9020), so guess I did not misunderstand: 
+def over_nine_thousand(lst):
+  sum = 0
+  for number in lst:
+    sum += number
+    if (sum > 9000):
+      break
+  return sum
+
 
 #Uncomment the line below when your function is done
 print(over_nine_thousand([8000, 900, 120, 5000]))
