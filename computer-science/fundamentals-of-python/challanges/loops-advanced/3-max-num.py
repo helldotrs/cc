60b91720c0bce08d75bba5befe5232292f0b3d0e
Rebekah <macrosystem@proton.me>
2023-07-03 03:15:36 +0200

    Update 3-max-num.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/3-max-num.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/3-max-num.py
@@ -7,6 +7,14 @@ def max_num(list):
       max = item
   return max
 
+#I don't like CCs solution as it first sets maximum to nums[0] and then compares nums[0] to nums[0] in the first iteration. 
+def max_num(nums):
+  maximum = nums[0]
+  for number in nums:
+    if number > maximum:
+      maximum = number
+  return maximum
+
 
 #Uncomment the line below when your function is done
 print(max_num([50, -10, 0, 75, 20]))
