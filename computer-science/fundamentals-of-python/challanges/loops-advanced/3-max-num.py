45ba0f18029cb398eedb3f159c1b65c5d474373b
Rebekah <macrosystem@proton.me>
2023-07-03 03:13:53 +0200

    Update 3-max-num.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/3-max-num.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/3-max-num.py
@@ -1,5 +1,12 @@
 #Write your function here
 
+def max_num(list):
+  max = 0;
+  for item in list:
+    if item > max:
+      max = item
+  return max
+
 
 #Uncomment the line below when your function is done
-#print(max_num([50, -10, 0, 75, 20]))
+print(max_num([50, -10, 0, 75, 20]))
