c517a10afd27820e031701cd4054ca562452f1a3
Rebekah <macrosystem@proton.me>
2023-07-06 13:14:35 +0200

    Update 4-average.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/4-average.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/4-average.py
@@ -1,7 +1,12 @@
 # Write your average function here:
 
+def average(a,b):
+  steps = abs(a-b)
+
+  return steps
+
 # Uncomment these function calls to test your average function:
-#print(average(1, 100))
+print(average(1, 100))
 # The average of 1 and 100 is 50.5
-#print(average(1, -1))
+print(average(1, -1))
 # The average of 1 and -1 is 0
