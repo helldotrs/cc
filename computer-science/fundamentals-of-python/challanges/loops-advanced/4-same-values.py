880e8e543b6f8b85e0b5d47f7e6024c8f6dc045e
Rebekah <macrosystem@proton.me>
2023-07-08 23:19:47 +0200

    Update 4-same-values.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/4-same-values.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/4-same-values.py
@@ -3,6 +3,10 @@
 def same_values(a,b):
   pass
   output  = []
+  for i in a:
+    if a[i] == b[i]:
+      output.append[i]
+  return output
 
 #Uncomment the line below when your function is done
-#print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
