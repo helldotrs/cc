bdd1e767063dea567db1d6fd26af924323ae2836
Rebekah <macrosystem@proton.me>
2023-07-09 11:43:38 +0200

    Update 5-reversed-list.py

--- computer-science/fundamentals-of-python/challanges/loops-advanced/5-reversed-list.py
+++ computer-science/fundamentals-of-python/challanges/loops-advanced/5-reversed-list.py
@@ -1,6 +1,8 @@
 #Write your function here
 
+def reversed_list(a,b):
+  
 
 #Uncomment the lines below when your function is done
-#print(reversed_list([1, 2, 3], [3, 2, 1]))
-#print(reversed_list([1, 5, 3], [3, 2, 1]))
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
