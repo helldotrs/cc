2f5b87631608f5425565dd505657e34994218f38
Rebekah <macrosystem@proton.me>
2023-07-14 01:54:16 +0200

    Update 2-square-root.py

--- computer-science/fundamentals-of-python/challanges/functions/2-square-root.py
+++ computer-science/fundamentals-of-python/challanges/functions/2-square-root.py
@@ -1,7 +1,10 @@
 # Write your square_root function here:
 
+def square_root(a):
+  return a ** 0.5
+
 # Uncomment these function calls to test your square_root function:
-#print(square_root(16))
+print(square_root(16))
 # should print 4
-#print(square_root(100))
+print(square_root(100))
 # should print 10
