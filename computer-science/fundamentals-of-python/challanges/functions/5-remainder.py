184c4b864502abf4cf1625ca4adfa8cab597b1f6
Rebekah <macrosystem@proton.me>
2023-07-16 00:42:02 +0200

    Update 5-remainder.py

--- computer-science/fundamentals-of-python/challanges/functions/5-remainder.py
+++ computer-science/fundamentals-of-python/challanges/functions/5-remainder.py
@@ -1,5 +1,8 @@
 # Write your remainder function here:
 
+def remainder(num1, num2):
+  return (num1*2) % (num2/2)
+
 # Uncomment these function calls to test your remainder function:
 #print(remainder(15, 14))
 # should print 2
