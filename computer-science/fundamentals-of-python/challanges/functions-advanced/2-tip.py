246480fdfd8c019566bfe1eb560d4521115d5977
Rebekah <macrosystem@proton.me>
2023-07-17 18:16:45 +0200

    Update 2-tip.py

--- computer-science/fundamentals-of-python/challanges/functions-advanced/2-tip.py
+++ computer-science/fundamentals-of-python/challanges/functions-advanced/2-tip.py
@@ -1,1 +1,9 @@
+# Write your tip function here:
 
+def tip(a)
+  
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
