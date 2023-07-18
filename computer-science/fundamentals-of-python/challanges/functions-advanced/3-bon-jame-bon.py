033fcc91f5737191e8cc760c1c8b2a186032d76f
Rebekah <macrosystem@proton.me>
2023-07-18 15:48:15 +0200

    Update 3-bon-jame-bon.py

--- computer-science/fundamentals-of-python/challanges/functions-advanced/3-bon-jame-bon.py
+++ computer-science/fundamentals-of-python/challanges/functions-advanced/3-bon-jame-bon.py
@@ -4,7 +4,7 @@ def introduction(first_n, sec_n):
   return sec_n + ", " + first_n + " " + sec_n
 
 # Uncomment these function calls to test your introduction function:
-#print(introduction("James", "Bond"))
+print(introduction("James", "Bond"))
 # should print Bond, James Bond
-#print(introduction("Maya", "Angelou"))
+print(introduction("Maya", "Angelou"))
 # should print Angelou, Maya Angelou
