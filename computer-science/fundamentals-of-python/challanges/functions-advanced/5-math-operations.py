c8d3120a3b1dce0bb8bcbaf260aa58be5fe2d35c
Rebekah <macrosystem@proton.me>
2023-07-18 16:09:05 +0200

    Update 5-math-operations.py

--- computer-science/fundamentals-of-python/challanges/functions-advanced/5-math-operations.py
+++ computer-science/fundamentals-of-python/challanges/functions-advanced/5-math-operations.py
@@ -1,3 +1,5 @@
+#this code is ugly and I am not proud of it, but it works.
+
 """
 First, print the sum of a and b.
 
@@ -21,7 +23,7 @@ def lots_of_math(a,b,c,d):
 # Write your lots_of_math function here:
 
 # Uncomment these function calls to test your lots_of_math function:
-#print(lots_of_math(1, 2, 3, 4))
+print(lots_of_math(1, 2, 3, 4))
 # should print 3, -1, -3, 0
-#print(lots_of_math(1, 1, 1, 1))
+print(lots_of_math(1, 1, 1, 1))
 # should print 2, 0, 0, 0
