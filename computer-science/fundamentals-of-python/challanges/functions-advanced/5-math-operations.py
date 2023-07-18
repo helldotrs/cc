e60d570cec786a0722e26a3b6e470c67ed44ac2a
Rebekah <macrosystem@proton.me>
2023-07-18 16:01:48 +0200

    Create 5-math-operations.py

---
+++ computer-science/fundamentals-of-python/challanges/functions-advanced/5-math-operations.py
@@ -0,0 +1,17 @@
+"""
+First, print the sum of a and b.
+
+Second, print c minus d.
+
+Third, print the first number printed, multiplied by the second number printed.
+
+Finally, return the third number printed modulo a.
+"""
+
+# Write your lots_of_math function here:
+
+# Uncomment these function calls to test your lots_of_math function:
+#print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+#print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
