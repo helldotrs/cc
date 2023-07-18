4c9d8fa227a54c38574e1599fcf9a9642f7c82b0
Rebekah <macrosystem@proton.me>
2023-07-18 15:56:33 +0200

    Create 4-dog-years.py

---
+++ computer-science/fundamentals-of-python/challanges/functions-advanced/4-dog-years.py
@@ -0,0 +1,10 @@
+# Write your dog_years function here:
+
+def dog_years(name,age):
+  return name + " is " + str(age * 7) + " in dog years"
+
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
